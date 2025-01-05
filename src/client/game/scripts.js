9dd014fcabf28c7f7fbd1fb718975a82bb3029df
nikitakaralius <nikitakaralyus@gmail.com>
2025-01-05 23:27:08 +0300

    Extracted preventUnplannedGame function

--- src/client/game/scripts.js
+++ src/client/game/scripts.js
@@ -1,14 +1,17 @@
-const gamePreferencesValue = sessionStorage.getItem(SESSION_STORAGE_KEYS.GAME_PREFERENCES);
-const gamePreferences = JSON.parse(gamePreferencesValue);
+preventUnplannedGame();
 
-if (!gamePreferences || gamePreferences.gameStarted) {
-  redirectToPage("menu");
+function preventUnplannedGame() {
+  const gamePreferencesValue = sessionStorage.getItem(SESSION_STORAGE_KEYS.GAME_PREFERENCES);
+  const gamePreferences = JSON.parse(gamePreferencesValue);
+
+  if (!gamePreferences || gamePreferences.gameStarted) {
+    redirectToPage("menu");
+  }
+
+  gamePreferences.gameStarted = true;
+
+  sessionStorage.setItem(
+    SESSION_STORAGE_KEYS.GAME_PREFERENCES,
+    JSON.stringify(gamePreferences)
+  );
 }
-
-gamePreferences.gameStarted = true;
-
-sessionStorage.setItem(
-  SESSION_STORAGE_KEYS.GAME_PREFERENCES,
-  JSON.stringify(gamePreferences)
-);
-
