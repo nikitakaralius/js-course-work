0d23aa59e875b9f71c822649cbaf09501984d156
nikitakaralius <nikitakaralyus@gmail.com>
2025-01-05 14:44:30 +0300

    Added stinky version of default routing

--- src/client/shared/router.js
+++ src/client/shared/router.js
@@ -1,15 +1,32 @@
+redirectToDefaultPage();
+
+function redirectToDefaultPage() {
+  const currentUser = localStorage.getItem(LOCAL_STORAGE_KEYS.CURRENT_USER);
+
+  if (currentUser) {
+    redirectToPage("menu");
+  } else {
+    redirectToPage("auth");
+  }
+}
+
 function redirectToPage(page) {
+  if (window.location.pathname.includes(page))
+    return;
+
   const baseUrl = window.location.origin;
   const pathname = window.location.pathname;
 
-  const endsWithSlash = window.location.pathname.endsWith('/')
+  let newPathname;
 
-  const lastSlashIndex = pathname.lastIndexOf("/");
-  let pathnameWithoutLastPart = pathname.slice(0, lastSlashIndex);
-
-  if (endsWithSlash) {
-    pathnameWithoutLastPart = pathname.slice(0, pathnameWithoutLastPart.lastIndexOf("/"));
+  if (pathname.includes('menu')) {
+    newPathname = pathname.replace('menu', page);
+  } else if (pathname.includes('auth')) {
+    newPathname = pathname.replace('auth', page);
+  }
+  else {
+    newPathname = `${pathname}/${page}`;
   }
 
-  window.location.href = `${baseUrl}${pathnameWithoutLastPart}/${page}`;
+  window.location.href = `${baseUrl}${newPathname}`;
 }
