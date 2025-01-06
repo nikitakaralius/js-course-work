244cbf82591d3bffdfe8874c0cfb3074d3c2812d
nikitakaralius <nikitakaralyus@gmail.com>
2025-01-06 12:20:48 +0300

    Added timer view

--- src/client/game/scripts.js
+++ src/client/game/scripts.js
@@ -1,7 +1,21 @@
 startOrPreventGame();
 
 function startOrPreventGame() {
-  const game = AppContext.gameBuilder.build();
+  const timerBar = document.getElementById('timer-bar');
+
+  const timerHandlers = {
+    onTimeChanged: (percentage) => {
+      timerBar.style.width = percentage + '%';
+    },
+    onEnd: () => {
+      alert("Time's up!");
+    },
+  }
+
+  const game = AppContext
+    .gameBuilder
+    .setTimerHandlers(timerHandlers)
+    .build();
 
   if (!game) {
     AppContext.router.redirectToPage(PAGE.MENU);
